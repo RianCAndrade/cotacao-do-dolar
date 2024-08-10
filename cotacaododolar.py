3b8ac000d74c6ab816b3263bd691cf9dcf434483
RianCAndrade <riannewsouls@gmail.com>
2024-08-10 15:54:44 -0300

    Cotação do dolar

--- cotacaododolar.py
+++ cotacaododolar.py
@@ -6,7 +6,7 @@ import json
 cota = requests.get("https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL")
 cota = cota.json()
 cotadodolar = cota['USDBRL']['bid']
-#print(cota)
+
 print(cotadodolar)
 
 
